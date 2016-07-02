17f8e7089e5573dc6488985229bbe86d4e2bdb57
Zak Golding <zak@0x7cf.com>
2016-07-01 20:47:27 -0700

    added elliot

---
+++ app.js
@@ -0,0 +1,17 @@
+$(function() {
+  $('#nameplate').rainbow({
+    colors: [
+        '#FF0000',
+        '#f26522',
+        '#fff200',
+        '#00a651',
+        '#28abe2',
+        '#2e3192',
+        '#6868ff'
+    ],
+    animate: true,
+    animateInterval: 100,
+    pad: false,
+    pauseLength: 100,
+});
+});
